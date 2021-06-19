fe9a17b24743d4bc6e430fc58d33cd00308fb5ab
stephdraggo <61036246+stephdraggo@users.noreply.github.com>
2021-06-19 16:56:28 +1000

    raycast ew

--- Assets/SpaceSim/Scripts/Player/ShipInput.cs
+++ Assets/SpaceSim/Scripts/Player/ShipInput.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using SpaceSim.Mining;
+using UnityEditor;
 using UnityEngine;
 
 namespace SpaceSim.Ship
@@ -15,7 +18,23 @@ namespace SpaceSim.Ship
         [SerializeField]
         private float throttleSpeed = 0.5f, rollSpeed = 5;
 
+        [SerializeField]
+        private Transform shootPoint;
+
+        private Ray ray;
+        
         void Update() {
+            Move();
+
+
+            if (Input.GetKey(KeyCode.Mouse0)) {
+                RaycastClick();
+            }
+        }
+
+        #region move
+
+        private void Move() {
             strafe = Input.GetAxis("Horizontal");
 
             SetAngleDirectionMouse();
@@ -24,8 +43,6 @@ namespace SpaceSim.Ship
             UpdateThrottleKeyboard(KeyCode.W, KeyCode.S);
             UpdateRollKeyboard(KeyCode.Q, KeyCode.E);
         }
-
-        #region throttle
 
         /// <summary>
         /// Get mouse wheel for accelerator and clamp value
@@ -50,7 +67,6 @@ namespace SpaceSim.Ship
             throttle = Mathf.MoveTowards(throttle, target, Time.deltaTime * throttleSpeed);
         }
 
-        #endregion
 
         private void SetAngleDirectionMouse() {
             Vector3 mousePos = Input.mousePosition;
@@ -77,5 +93,34 @@ namespace SpaceSim.Ship
 
             roll = Mathf.MoveTowards(roll, target, Time.deltaTime * rollSpeed);
         }
+
+        #endregion
+
+        #region interact
+
+        //hecc
+        private void RaycastClick() {
+            Vector3 from = shootPoint.position;
+            Vector3 to = from + shootPoint.forward * 10;
+            ray = new Ray(from, to);
+            if (Physics.Raycast(ray, out RaycastHit hit, 50)) {
+                if (hit.collider.TryGetComponent(out Hittable hittable)) {
+                    if (hittable is Asteroid) {
+                        //asteroid
+                        Debug.Log(hittable.name + " is an asteroid");
+                    }
+                    else {
+                        //enemy
+                    }
+                }
+            }
+        }
+
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(ray.origin,ray.direction);
+        }
+
+        #endregion
     }
 }
